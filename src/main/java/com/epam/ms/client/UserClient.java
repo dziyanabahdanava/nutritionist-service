652e710dedf42f8358e0374d96d597162d6ceafa
Dziyana Bahdanava <dziyana_bahdanava@epam.com>
2020-09-23 23:03:12 +0300

    Module 5

--- src/main/java/com/epam/ms/client/UserClient.java
+++ src/main/java/com/epam/ms/client/UserClient.java
@@ -1,21 +1,17 @@
 package com.epam.ms.client;
 
 import com.epam.ms.service.calculator.model.UserProfile;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
-import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
-@FeignClient(value = "user-service", url = "http://localhost:8080", path = "/users")
+@FeignClient(value = "user-service", url = "${clients.user-service.url}", path = "/users")
 public interface UserClient {
 
+    //@LoadBalanced
     @RequestMapping(method = GET, value = "/{id}/profiles")
     UserProfile findProfileByUserId(@PathVariable String id);
-
-    // Method is not used so far
-    @RequestMapping(method = POST)
-    void create(UserProfile userProfile);
-
 }
