a7206cca620cfe186337bb795762fb42de69de10
Dziyana Bahdanava <dziyana_bahdanava@epam.com>
2020-09-30 14:41:51 +0300

    Get rid of extra dependencies

--- src/main/java/com/epam/ms/client/UserClient.java
+++ src/main/java/com/epam/ms/client/UserClient.java
@@ -1,17 +1,15 @@
 package com.epam.ms.client;
 
 import com.epam.ms.service.calculator.model.UserProfile;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
-@FeignClient(value = "user-service", url = "${clients.user-service.url}", path = "/users")
+@FeignClient(value = "user-service", path = "/users")
 public interface UserClient {
 
-    //@LoadBalanced
     @RequestMapping(method = GET, value = "/{id}/profiles")
     UserProfile findProfileByUserId(@PathVariable String id);
 }
